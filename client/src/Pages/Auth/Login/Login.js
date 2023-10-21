c3c71b9a6b6e76bf8424dde218ba5d0f2381a2b1
Anas <110332632+anasAS02@users.noreply.github.com>
2023-10-21 21:26:19 +0300

    .

--- client/src/Pages/Auth/Login/Login.js
+++ client/src/Pages/Auth/Login/Login.js
@@ -11,7 +11,7 @@ import LoadingSpinner from '../../../Components/LoadingSpinner/LoadingSpinner';
 export default function Login(){
     const {isLoggedIn} = useAuth();
     const [error, setError] = useState(null);
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
 
     const formik = useFormik({
             initialValues: {

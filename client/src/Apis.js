e35e327dabf0c67c57cfa28f7d5af750e5ecef5e
Anas <110332632+anasAS02@users.noreply.github.com>
2024-01-13 11:16:57 +0200

    .

--- client/src/Apis.js
+++ client/src/Apis.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://audiophile-yv5c.onrender.com/';
+export const BASE_URL = 'https://audiophile-production.up.railway.app/';
 export const SIGNUP = `${BASE_URL}auth/signup`;
 export const LOGIN = `${BASE_URL}auth/login`;
 export const REFRESH_TOKEN = `${BASE_URL}auth/refresh`;
